a17af3a57b6ef22c85360e2ba7e70ffe095002ed
Emmanuel <emanueldelidakis@gmail.com>
2026-04-02 18:36:17 +0300

    Main and System.in

---
+++ src/Part1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Part1{
+
+    public static void main(String[] args) throws IOException {
+        // Create an instance of Part1 and pass System.in as the input stream
+        Part1 evaluator = new Part1(System.in);
+    }
+}
